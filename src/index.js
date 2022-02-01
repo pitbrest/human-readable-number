df48abe39752f18454a773f241ffccbb576d09e3
pitbrest <sasha.petrov@tut.by>
2022-02-01 23:56:45 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable(number) {
 
-	const sup1 = { 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eightteen", 19: "nineteen"}
-	const sup2 = { 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety"}
-	const sup3 = { 100: "hundred", 1000: "thousand", 1000000: "million"}
+	const sup1 = { 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eightteen", 19: "nineteen" }
+	const sup2 = { 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety" }
+	const sup3 = { 100: "hundred", 1000: "thousand", 1000000: "million" }
 
 	if (number == 0) { return "zero" }
 	if (number <= 19 && number > 0) { return sup1[number] }
@@ -19,6 +19,9 @@ module.exports = function toReadable(number) {
 			return sup1[help1[0]] + ' ' + sup3[100]
 		}
 		if (number % 10 == 0) {
+			return sup1[help1[0]] + ' ' + sup3[100] + ' ' + toReadable(Number(number.toString().slice(1)))
+		}
+		if (Number(number.toString().slice(0, 2)) % 10 == 0) {
 			return sup1[help1[0]] + ' ' + sup3[100] + ' ' + toReadable(Number(number.toString().slice(1)))
 		}
 		else {
